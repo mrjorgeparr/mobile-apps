edc98ce32fb4d7576caa6c0e620e8c50e00a0093
MNF2001 <marconarganesf@gmail.com>
2022-05-06 17:05:54 +0200

    ...

--- Funniflier2/app/src/main/java/com/example/funniflier2/db2/User.java
+++ Funniflier2/app/src/main/java/com/example/funniflier2/db2/User.java
@@ -22,11 +22,19 @@ public class User
     @ColumnInfo(name="name")
     private String name;
 
+    @ColumnInfo(name="surname1")
+    private String surname1;
+
+    @ColumnInfo(name="surname2")
+    private String surname2;
+
     public User(String email, String password, String name)
     {
         this.email = email;
         this.password = password;
         this.name = name;
+        this.surname1 = surname1;
+        this.surname2 = surname2;
     }
 
     //Getters
@@ -34,10 +42,14 @@ public class User
     public String getName(){ return this.name; }
     public String getEmail(){ return this.email; }
     public String getPassword(){ return this.password; }
+    public String getSurname1(){ return this.surname1; }
+    public String getSurname2(){ return this.surname2; }
 
     //Setters
     public void setId(long id){ this.id = id; }
     public void setName(String name){ this.name = name; }
     public void setEmail(String Email){ this.email = email; }
     public void setPassword(String password){ this.password = password; }
+    public void setSurname1(String surname1){ this.email = surname1; }
+    public void setSurname2(String surname2){ this.password = surname2; }
 }
