ec3f2596c05ebc4d6eea526e6674ad5e9fd4d013
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2022-05-10 12:24:27 +0200

    doing progress

--- Funniflier2/app/src/main/java/com/example/funniflier2/RegisterBusinessActivity.java
+++ Funniflier2/app/src/main/java/com/example/funniflier2/RegisterBusinessActivity.java
@@ -1,9 +1,15 @@
 package com.example.funniflier2;
 
 
+import static com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,14 +19,34 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.funniflier2.db.Business;
 import com.example.funniflier2.db.DB;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
 
 public class RegisterBusinessActivity extends AppCompatActivity implements View.OnClickListener {
     // defines the button as a member of the class
     Button ok_button;
+    int LOCATION_REFRESH_TIME = 15000; // 15 seconds to update
+    int LOCATION_REFRESH_DISTANCE = 500; // 500 meters to update
+    float long_, lat;
+
+    LocationManager mLocationManager;
+    FusedLocationProviderClient fusedLocationClient;
+    final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            //your code here
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +55,44 @@ public class RegisterBusinessActivity extends AppCompatActivity implements View.
         ok_button = findViewById(R.id.ok_button);
         ok_button.setOnClickListener(this);
         //Button ok_button = (Button) findViewById(R.id.ok_button);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        CancellationToken token = new CancellationToken() {
+            @NonNull
+            @Override
+            public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
+                        return null;
+            }
+
+            @Override
+            public boolean isCancellationRequested() {
+                return false;
+            }
+        };
+        fusedLocationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, token)
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            long_ = (float) location.getLatitude();
+                            lat = (float) location.getLatitude();
+                        }
+                    }
+                });
+
+
 
 
     }
@@ -56,7 +120,8 @@ public class RegisterBusinessActivity extends AppCompatActivity implements View.
             DB db=DB.getInstance(this);
 
             try {
-                Business business = new Business(name, email, password, address, is_hairdresser, 0, 0, schedule, 0);
+
+                Business business = new Business(name, email, password, address, is_hairdresser, long_, lat, schedule, 4f);
                 db.businessDao().insert(business);
 
                 Intent intent2 = new Intent(this, HomeBusinessActivity.class);
