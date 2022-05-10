61b89206df8139601b82c81bd68a6664ef156b66
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2022-05-10 06:21:31 +0200

    working on scanner activity

--- Funniflier2/app/src/main/java/com/example/funniflier2/ScannerActivity.java
+++ Funniflier2/app/src/main/java/com/example/funniflier2/ScannerActivity.java
@@ -1,5 +1,7 @@
 package com.example.funniflier2;
 
+import com.example.funniflier2.db.Business;
+import com.example.funniflier2.db.DB;
 import com.google.zxing.client.android.Intents;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
@@ -14,7 +16,8 @@ import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 public class ScannerActivity extends AppCompatActivity {
-
+    DB db = DB.getInstance(this);
+    long user_id;
     private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
             result -> {
                 if(result.getContents() == null) {
@@ -27,14 +30,36 @@ public class ScannerActivity extends AppCompatActivity {
                         Toast.makeText(ScannerActivity.this, "Cancelled due to missing camera permission", Toast.LENGTH_LONG).show();
                     }
                 } else {
+
+                    long business_id = Long.getLong(result.getContents());
+                    try{
+                        Business b = db.businessDao().findById(business_id);
+
+                        Intent intent2 = new Intent(this, BusinessActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putLong("business_id", b.getId());
+                        bundle.putLong("user_id", user_id);
+                        intent2.putExtras(bundle);
+                        this.startActivity(intent2);
+
+                    }catch(Exception e){
+                        Toast.makeText(ScannerActivity.this, "Not found", Toast.LENGTH_LONG).show();
+                        Intent intent2 = new Intent(this, HomeActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putLong("user_id", user_id);
+                        intent2.putExtras(bundle);
+                        this.startActivity(intent2);
+                    }
                     Log.d("ScannerActivity", "Scanned");
-                    Toast.makeText(ScannerActivity.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+
                 }
             });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        user_id = getIntent().getExtras().getLong("user_id");
         ScanOptions options = new ScanOptions();
         options.setCaptureActivity(AnyOrientationCaptureActivity.class);
         options.setDesiredBarcodeFormats(ScanOptions.ONE_D_CODE_TYPES);
@@ -43,4 +68,5 @@ public class ScannerActivity extends AppCompatActivity {
         options.setBeepEnabled(false);
         barcodeLauncher.launch(options);
     }
+
 }
